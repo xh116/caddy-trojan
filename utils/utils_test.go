63affae063f10b40af87ee25b75505d1769a7cd4
xh116 <torrencechen@gmail.com>
2024-04-25 00:51:56 +0800

    new

---
+++ utils/utils_test.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestByteSliceToString(t *testing.T) {
+	for _, v := range []string{
+		"test1234",
+	} {
+		if ByteSliceToString([]byte(v)) != v {
+			t.Errorf("convert error: %v", v)
+		}
+	}
+}
+
+func TestStringToByteSlice(t *testing.T) {
+	for _, v := range []string{
+		"test1234",
+	} {
+		if !bytes.Equal([]byte(v), StringToByteSlice(v)) {
+			t.Errorf("convert error: %v", v)
+		}
+	}
+}
